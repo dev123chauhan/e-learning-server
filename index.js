8f77491374bf2dfc3c8794ae6c2ccc061cd5a16a
dev123chauhan <76210158+dev123chauhan@users.noreply.github.com>
2024-10-14 23:17:53 +0530

    demo3

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ app.use("/api", userRoutes);
 app.use("/api", coursesRoute);
 app.use("/api", googleRoute);
 app.use('/api', enrollmentRoutes);
+
+mongoose.connect(process.env.MONGODB_URI, { })
+  .then(() => console.log("MongoDB database connection established successfully"))
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+
+  
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
